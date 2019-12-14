2244608e0d63a3577a1cf6ee28672f914c4eb8b1
hemant pande <hemant.pande87@gmailcom>
2019-12-14 14:31:43 +0530

    Player 1 plays again when maximum is scored with incremental positions

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -9,8 +9,8 @@ public class Board {
 
     public Board() {
         playerPositions = new HashMap<Integer, Integer>() {{
-            put(1, 1);
-            put(2, 1);
+            put(1, 0);
+            put(2, 0);
         }};
         currentPlayer = 1;
     }
@@ -20,13 +20,20 @@ public class Board {
     }
 
     public String play(int dice1, int dice2) {
-        playerPositions.replace(currentPlayer, dice1 + dice2);
+        final int newPosition = getNewPosition(dice1, dice2);
 
-        final String message = String.format("Player %s is on square %s", currentPlayer, playerPositions.get(currentPlayer));
+        playerPositions.replace(currentPlayer, newPosition);
+
+        final String message = String.format("Player %s is on square %s", currentPlayer, newPosition);
 
         decideNextPlayer(dice1 + dice2);
 
         return message;
+    }
+
+    private int getNewPosition(int dice1, int dice2) {
+        final Integer currentPosition = playerPositions.get(currentPlayer);
+        return currentPosition + dice1 + dice2;
     }
 
     private void decideNextPlayer(int totalpoints) {
--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BoardShould {
 
     @Test
-    @DisplayName("Both players start at square 1")
-    void makeBothPlayersStartAtDefaultPosition() {
+    @DisplayName("Both players start at default position")
+    void make_Both_Players_Start_At_Default_Position() {
         Board board = new Board();
 
-        assertAll(() -> assertEquals(1, board.position(1)),
-                  () -> assertEquals(1, board.position(2)));
+        assertAll(() -> assertEquals(0, board.position(1)),
+                  () -> assertEquals(0, board.position(2)));
     }
 
     @Test
     @DisplayName("Player 1 should make a normal move")
-    void letPlayer1MakeTheFirstMove() {
+    void let_Player1_Make_The_First_Move() {
         Board board = new Board();
 
         assertEquals("Player 1 is on square 3", board.play(1, 2));
@@ -26,10 +26,20 @@ public class BoardShould {
 
     @Test
     @DisplayName("Player 2 should play after player 1 when both dice are not maximum")
-    void letPlayer2PlayWhenBothDiceAreNotMaximum() {
+    void let_Player2_Play_When_Both_Dice_Are_Not_Maximum() {
         Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 3", board.play(1, 2)),
                   () -> assertEquals("Player 2 is on square 3", board.play(1,2)));
+    }
+
+
+    @Test
+    @DisplayName("Player 1 should play again when both dice are maximum")
+    void let_Player1_Play_Again_When_Maximum_Is_Scored() {
+        Board board = new Board();
+
+        assertAll(() -> assertEquals("Player 1 is on square 12", board.play(6, 6)),
+                () -> assertEquals("Player 1 is on square 24", board.play(6, 6)));
     }
 }
