ffd3789e8af951fa493509478b7d11002a44eb0b
hemant pande <hemant.pande87@gmailcom>
2019-12-14 17:18:29 +0530

    removing code duplication by setup method

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardShould {
 
+    Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
     @Test
     @DisplayName("Both players start at default position")
     void make_Both_Players_Start_At_Default_Position() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals(0, board.position(1)),
                 () -> assertEquals(0, board.position(2)));
@@ -18,7 +25,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 should make a normal move")
     void let_Player1_Make_The_First_Move() {
-        Board board = new Board();
 
         assertEquals("Player 1 is on square 3", board.play(1, 2));
     }
@@ -27,7 +33,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 2 should play after player 1 when both dice are not maximum")
     void let_Player2_Play_When_Player1_does_not_score_same_die() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 3", board.play(1, 2)),
                 () -> assertEquals("Player 2 is on square 3", board.play(1, 2)));
@@ -36,7 +41,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 should play again when both dice are maximum")
     void let_Player1_Play_Again_When_same_die_Is_Scored() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 4", board.play(2, 2)),
                 () -> assertEquals("Player 1 is on square 14", board.play(6, 4)));
@@ -45,7 +49,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 2 should play again when both dice are maximum")
     void let_Player2_Play_Again_When_same_die_Is_Scored() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 11", board.play(5, 6)),
                 () -> assertEquals("Player 2 is on square 12", board.play(6, 6)),
@@ -55,7 +58,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 should take the ladder when encountered")
     void let_Player1_take_the_ladder_when_encountered() {
-        Board board = new Board();
 
         assertEquals("Player 1 is on square 38", board.play(1, 1));
     }
@@ -63,7 +65,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 bounces back when roll is high, near the finish line")
     void let_Player1_bounce_back_when_roll_is_too_high_near_the_finish_line() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 38", board.play(1, 1)),
                 () -> assertEquals("Player 1 is on square 48", board.play(6, 4)),
@@ -77,7 +78,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 bounces back when roll is high, near the finish line, and gets a snake")
     void let_Player1_bounce_back_when_roll_is_too_high_near_the_finish_line_and_gets_a_snake() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 38", board.play(1, 1)),
                 () -> assertEquals("Player 1 is on square 48", board.play(6, 4)),
@@ -91,7 +91,6 @@ public class BoardShould {
     @Test
     @DisplayName("Player 1 wins")
     void let_Player1_win_when_exact_number_of_dice_is_scored() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 38", board.play(1, 1)),
                 () -> assertEquals("Player 1 is on square 48", board.play(6, 4)),
@@ -105,7 +104,6 @@ public class BoardShould {
     @Test
     @DisplayName("Game over")
     void not_allow_Player2_to_play_when_player1_has_already_won() {
-        Board board = new Board();
 
         assertAll(() -> assertEquals("Player 1 is on square 38", board.play(1, 1)),
                 () -> assertEquals("Player 1 is on square 48", board.play(6, 4)),
